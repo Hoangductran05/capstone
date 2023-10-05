6f51f11e566a2b629c88b416d20d8752cba1a4b9
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-05 15:09:28 -0500

    product background

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -12,7 +12,7 @@ const Products = ({product}) => {
 
   return (
     <div>
-      <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
+      <div className=' bg-white h-[300px] mb-4 relative overflow-hidden group transition'>
         <div className='w-full h-full flex justify-center items-center'>
           {/* {image} */}
           <div className='w-[200px] mx-auto flex justify-center items-center'>
