049bb9d4a33a935d757e7b8948cf65cf7aba1ebd
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-05 08:24:48 -0500

    cart styling

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -2,12 +2,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { IoMdClose } from 'react-icons/io';
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 
 
 const Cartitems = ({item}) => {
   const {id, title, image, price, amount} = item;
-  return <div className='flex'>
+  return <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
     <div className='w-full min-h-[150px] flex items-center gap-x-4'>
       {/* image */}
       <Link to={`/product/${id}`}>
@@ -24,13 +24,31 @@ const Cartitems = ({item}) => {
             <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
           </div>
         </div>
-        <div className='bg-stone-400 flex gap-x-2 h-[36px] text-sm'>
+        <div className=' flex gap-x-2 h-[36px] text-sm'>
           {/* qty */}
-          <div>quantity</div>
+          <div className='flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
+            
+            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+              {/* minus icon */}
+              <IoMdRemove />
+            </div>
+
+            {/* amount */}
+            <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+
+            <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+              {/* plus icon */}
+              <IoMdAdd />
+            </div>
+
+          </div>
           {/* price */}
-          <div>item price</div>
+          <div className='flex-1 flex items-center justify-around'>$ {price}</div>
+
           {/* final price */}
-          <div>final price</div>
+          <div className='flex-1 flex justify-end items-center text-primary font-medium'>
+            {`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
+
         </div>
       </div>
     </div>
