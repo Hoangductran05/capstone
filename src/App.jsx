c0b4e85a4b67d0f7d7c436acf44492c91036a442
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-04 19:52:15 -0500

    import pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/cart'
+import Checkout from './pages/checkout'
+import Detail from './pages/detail'
+import Login from './pages/login'
+import Signup from './pages/signup'
+
 
 
 function App() {
