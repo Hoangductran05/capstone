ed79b8f0a5870e1c9ec4d9f39743476a0998f57e
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-05 07:57:13 -0500

    remove icon

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -1,10 +1,32 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { IoMdClose } from 'react-icons/io';
 
-const Cartitems = () => {
-  return (
-    <div>Cartitems</div>
-  )
+
+const Cartitems = ({item}) => {
+  const {id, title, image, price, amount} = item;
+  return <div className='flex'>
+    <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+      {/* image */}
+      <Link to={`/product/${id}`}>
+        <img className='max-w-[80px]' src={image} />
+      </Link>
+      <div className='w-full flex flex-col'>
+        {/* title and remove icon */}
+        <div className='flex justify-between mb-2'>
+          <Link className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline' to={`/product/${id}`}>
+            {title}
+          </Link>
+          {/* remove icon */}
+          <div className='text-xl cursor-pointer'>
+            <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 }
 
 export default Cartitems
