3286bda03bb9a98c95e7b81704c76b0f615d8b59
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-04 20:12:30 -0500

    context

---
+++ src/contexts/cartContext.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const CartContext = () => {
+  return (
+    <div>CartContext</div>
+  )
+}
+
+export default CartContext
---
+++ src/contexts/productContext.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const ProductContext = () => {
+  return (
+    <div>ProductContext</div>
+  )
+}
+
+export default ProductContext
