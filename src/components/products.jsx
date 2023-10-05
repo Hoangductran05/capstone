a80105de269c8bd6bb3c9c47cf870adf25c23f4d
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-04 22:24:49 -0500

    icons, products

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useContext} from 'react'
+import { Link } from 'react-router-dom'
+import {BsPlug, BsEyeFill} from 'react-icons/bs'
 
-const Products = () => {
+const Products = ({product}) => {
+  console.log(product)
   return (
     <div>Products</div>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, {useContext} from 'react'
 import { ProductContext } from '../contexts/productContext'
+import Products from '../components/products'
 
 const Home = () => {
   const { products } = useContext(ProductContext)
@@ -9,9 +10,11 @@ const Home = () => {
     <div>
       <section className='py-16'>
         <div className="container mx-auto">
-          <div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
             {products.map((product) => {
-              return <div key={product.id}>{product.title}</div>
+              return (
+                <Products product={product} key={product.id} />
+              )
             })}
           </div>
         </div>
