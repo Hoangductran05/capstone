ec1f3aefe520d7ad3cb157d67efc3c38e9056ea4
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-04 21:15:07 -0500

    tailwind

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useContext} from 'react'
+import { ProductContext } from '../contexts/productContext'
 
 const Home = () => {
+  const { products } = useContext(ProductContext)
+
   return (
-    <div>Home</div>
+    <div>
+      <section className='py-16'>
+        <div className="container mx-auto">
+          <div>
+            {products.map((product) => {
+              return <div key={product.id}>{product.title}</div>
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Poppins',
+    },
+    container: {
+      padding: {
+        DEFAULT: '30px',
+      }}}}
