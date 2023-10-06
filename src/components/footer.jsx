66b7b2a04f28907f06fdbb39d46f98e0e2e89567
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-05 21:38:10 -0500

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return <footer className='bg-primary py-12'>
     <div className="container mx-auto">
-      <p className=' text-white text-center'>Copyright &copy; Hoang Tran Ecommerce Shop 2023. All rights reserved.</p>
+      <p className=' text-white text-center'>Copyright &copy; Ecommerce Shop 2023. All rights reserved.</p>
     </div>
   </footer>
 }
