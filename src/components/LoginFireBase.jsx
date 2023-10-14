70176b74342c7407ba70dc361d17ef0ca7540fcb
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-14 07:56:41 -0500

    error message

--- src/components/LoginFireBase.jsx
+++ src/components/LoginFireBase.jsx
@@ -53,15 +53,16 @@ const Login2 = () => {
         />
         <label htmlFor="password">Your Password</label>
         </div>
-        {error && (
-                     <div className="alert alert-danger">{error}</div>
-                  )}
+        
      
         {/* button */}
         <button 
         className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2' 
         type='submit'>Login
         </button>
+
+        {/* error message */}
+        {error && (<div className="alert alert-danger">{error}</div>)}
 
         <div>
           <span>New Here? <Link className=' text-stone-500 hover:text-stone-900' to={'/register'}>Create An Account</Link></span>
--- src/components/Register2.jsx
+++ src/components/Register2.jsx
@@ -52,15 +52,16 @@ const Register2 = () => {
         />
         <label htmlFor="password">Your Password</label>
         </div>
-        {error && (
-                     <div className="alert alert-danger">{error}</div>
-                  )}
+        
      
         {/* button */}
         <button 
         className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2' 
         type='submit'>Register
         </button>
+
+        {/* error message */}
+        {error && (<div className="alert alert-danger">{error}</div>)}
 
         <div>
           <span>Already have an account? <Link className=' text-stone-500 hover:text-stone-900' to={'/login'}>Login</Link></span>
