9d93c089d664bbb0cc738a59418f05f639a07275
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-23 09:07:11 -0500

    checkout page

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import React from 'react'
 
 const Checkout = () => {
-  const [form, setForm] = useState({ name: '', email: '', password: '' });
+  const [form, setForm] = useState({ name: '', email: '', password: '', phone: '', address: '', city: ''});
 
   const handleChange = (e) => {
      setForm({ ...form, [e.target.name]: e.target.value });
@@ -29,6 +29,8 @@ const Checkout = () => {
         onChange={handleChange}
       />
       </div>
+
+      <div className=" relative my-4">
       <input
         className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
         type="email"
@@ -37,18 +39,34 @@ const Checkout = () => {
         value={form.email}
         onChange={handleChange}
       />
+      </div>
+
+      <div className=" relative my-4">
       <input
         className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={form.password}
+        type="phone"
+        name="tel"
+        placeholder="Phone"
+        value={form.phone}
         onChange={handleChange}
       />
+      </div>
+
+      <div className=" relative my-4">
+      <input
+        className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
+        type="address"
+        name="text"
+        placeholder="Address"
+        value={form.address}
+        onChange={handleChange}
+      />
+      </div>
+
       {/* button */}
       <button
        className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2'
-      type="submit">Submit</button>
+      type="submit">Pay</button>
     </form>
     </div>
   </div>
