b17bca9f95d8d9894f6c6fd3191beffe0d05ce52
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-23 08:59:44 -0500

    checkout page styling

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -14,17 +14,23 @@ const Checkout = () => {
      // submission logic here
   };
   return (
-    <div> 
-    <h1>Checkout</h1>
+    <div className=' text-stone-500 h-[100vh] flex justify-center items-center'> 
+    <div className=' bg-stone-800 border border-stone-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-20 relative '>
+    <h1 className='text-4x1 text-stone-600 font-bold text-center mb-6'>Checkout</h1>
     <form onSubmit={handleSubmit}>
+      
+      <div className=' relative my-4'>
       <input
+        className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
         type="text"
         name="name"
         placeholder="Name"
         value={form.name}
         onChange={handleChange}
       />
+      </div>
       <input
+        className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
         type="email"
         name="email"
         placeholder="Email"
@@ -32,14 +38,19 @@ const Checkout = () => {
         onChange={handleChange}
       />
       <input
+        className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
         type="password"
         name="password"
         placeholder="Password"
         value={form.password}
         onChange={handleChange}
       />
-      <button type="submit">Submit</button>
+      {/* button */}
+      <button
+       className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2'
+      type="submit">Submit</button>
     </form>
+    </div>
   </div>
   )
 }
