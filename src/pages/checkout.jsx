0a38aa0c6d3d9156070744b72507cc2419cc5acd
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-23 09:15:48 -0500

    check out page

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -27,6 +27,7 @@ const Checkout = () => {
         placeholder="Name"
         value={form.name}
         onChange={handleChange}
+        required
       />
       </div>
 
@@ -38,35 +39,38 @@ const Checkout = () => {
         placeholder="Email"
         value={form.email}
         onChange={handleChange}
+        required
       />
       </div>
 
       <div className=" relative my-4">
       <input
         className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
-        type="phone"
-        name="tel"
+        type="tel"
+        name="phone"
         placeholder="Phone"
         value={form.phone}
         onChange={handleChange}
+        required
       />
       </div>
 
       <div className=" relative my-4">
       <input
         className=' block w-72 py-2.3 px-0 text-sm text-primary bg-transparent border-0 border-b-2 border-x-gray-300 appearance-none dark:focus:border-stone-500 focus:outline-none focus:ring-0 focus:text-primary focus:border-stone-500 peer'
-        type="address"
-        name="text"
+        type="text"
+        name="address"
         placeholder="Address"
         value={form.address}
         onChange={handleChange}
+        required
       />
       </div>
 
       {/* button */}
       <button
        className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2'
-      type="submit">Pay</button>
+      type="submit">Pay Now</button>
     </form>
     </div>
   </div>
