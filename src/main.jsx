761bd50e38dd26a37268fba960f93668e949d880
Hoangductran05 <hoangducdoodle@gmail.com>
2023-10-30 23:55:07 -0500

    payment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 import App from './App.jsx'
 import './index.css'
 import ProductProvider from './contexts/productContext.jsx'
 import SidebarProvider from './contexts/sidebarContext.jsx'
 import CartProvider from './contexts/cartContext.jsx'
 
+const stripePromise = loadStripe('pk_test_51O79iBIavA56LkJMr3wbxdk6tLrlL2wfmuuHxJZANrhCSY5vMg5LiHRe55f4MqZvEZLeFuAPUuALgwsXgVcojlJ000416BZuyp')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SidebarProvider>
     <CartProvider>
       <ProductProvider>
         <React.StrictMode>
+          <Elements stripe={stripePromise}>
           <App />
+          </Elements>
         </React.StrictMode>
       </ProductProvider>
     </CartProvider>
--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -123,9 +123,17 @@ const Checkout = () => {
       </div>
 
       {/* button */}
-      <button
-       className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2'
-      type="submit">Pay Now</button>
+      <div className=" relative my-4">
+      <CardElement />
+      <button type="submit" disabled={!stripe}
+        className='w-full mb-4 text-[18px] mt-6 rounded-full bg-neutral-400 text-neutral-600 hover:bg-neutral-200 hover:text-stone-700 py-2'
+      >
+        Pay
+      </button>
+      </div>
+
+    
+    
     </form>
     </div>
   </div>
